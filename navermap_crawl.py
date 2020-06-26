87b86b3305a0fb2053048850d069e9eb097d14d1
dhkim <scminoov@gmail.com>
2020-06-26 11:38:33 +0900

    [Dev_Update] duplicated mistake, loc_name이 drop의 기준이 되서 문제가 발생

--- navermap_crawl.py
+++ navermap_crawl.py
@@ -31,6 +31,7 @@ def click_nolink_for_scrollDown(driver, scrollDown_num=100):
         sleep(0.1)
         body.send_keys(Keys.PAGE_DOWN)
 
+
 def crawl_data():
     df_rows = []
     # search = input("검색어를 입력하세요 : ")
@@ -85,64 +86,78 @@ def crawl_data():
     return save_path
 
 
-def review_crawl(crawled_data):
+def review_crawl(crawled_data, basis_column='loc_name'):
     linkdata = pd.read_csv(crawled_data, sep='\t')
     if 'Unnamed: 0' in linkdata.columns:
-        df_rows = linkdata.drop_duplicates(['loc_name'], ignore_index=True)
+        df_rows = linkdata.drop_duplicates([basis_column], ignore_index=True)
         df_rows = df_rows.drop('Unnamed: 0', axis=1)
     else:
-        df_rows = linkdata.drop_duplicates(['loc_name'], ignore_index=True)
+        df_rows = linkdata.drop_duplicates([basis_column], ignore_index=True)
 
     driver = webdriver.Chrome()
-    search = crawled_data.split('_')[-1]
+    search = crawled_data.split('_')[-1].split('.')[0]
+    final_rows = []
+    save_period = 10
     for idx, row in df_rows.iterrows():
-        row['reviews'] = []
-        row['scores'] = []
-        row['dates'] = []
         driver.get(row['link'])
         sleep(1)
 
         row['loc_name'] = driver.find_element_by_xpath('//*[@id="_title"]/span[1]').text
         print(row['loc_name'])
         sleep(1)
-
-        # more_receipt_x_path = '//*[@id="app-root"]/div/div[2]/div[4]/div/div[4]/div[2]/a'
         while 'receipt' not in driver.current_url:
-            # click_nolink_for_scrollDown(driver, 4)
-            # sleep(1)
             try:
-                receipt_review = driver.find_element_by_xpath('//*[@id="app-root"]/div/div[2]/div[3]/div/div/div/a[3]/span')
+                receipt_review = driver.find_element_by_xpath(
+                    '//*[@id="app-root"]/div/div[2]/div[3]/div/div/div/a[3]/span')
                 receipt_review.click()
             except Exception as e:
                 print(f'{e} ')
                 driver.get(row['link'])
-                # more_receipt_x_path = '/html/body/div[2]/div/div[2]/div[4]/div/div[4]/div[2]/a'
                 sleep(1)
             sleep(1)
+        try:
+            row['receipt_num'] = int(driver.find_element_by_class_name('place_section_count').text)
+            print(f'receipt_num : {row["receipt_num"]}')
+        except Exception as e:
+            print('receipt_num error', e)
+
+        row['reviews'] = []
+        row['scores'] = []
+        row['dates'] = []
         try:
             more_receipt = driver.find_element_by_xpath('//*[@id="app-root"]/div/div[2]/div[4]/div[2]/div[2]/div[2]/a')
             while more_receipt.is_enabled():
                 more_receipt.click()
-                sleep(0.3)
+                sleep(0.4)
         except StaleElementReferenceException as e:
             print(f'{e}, it`s ok .. go to next link')
+        except NoSuchElementException as e:
+            print(f'{e}, may be no review... it`s ok .. go to next link')
 
         try:
             review_elems = driver.find_elements_by_xpath(
                 '//*[@id="app-root"]/div/div[2]/div[4]/div[2]/div[2]/div/ul/li/div/div[2]/a/span')
-            review_scores = driver.find_elements_by_xpath(
-                '//*[@id="app-root"]/div/div[2]/div[4]/div[2]/div[2]/div/ul/li/div[2]/div[1]/span[2]')
-            review_dates = driver.find_elements_by_xpath(
-                '//*[@id="app-root"]/div/div[2]/div[4]/div[2]/div[2]/div/ul/li/div[2]/div[3]/div/div/span[1]')
-            for elem, score, date in zip(review_elems, review_scores, review_dates):
-                row['reviews'].append(elem.text)
-                row['scores'].append(score.text)
-                row['dates'].append(date.text)
+            review_scores = driver.find_elements_by_class_name('_3qIdi')
+            review_infos = driver.find_elements_by_class_name('_2wZjV')
+            review_dates = [x for i, x in enumerate(review_infos) if i % 3 == 1]
+            visit_nums = [x for i, x in enumerate(review_infos) if i % 3 == 2]
+            print(f'receipt_elem_num : {len(review_elems)}, {len(review_scores)}, {len(review_dates)}')
+            row['reviews'] = {i: elem.text for i, elem in enumerate(review_elems)}
+            row['scores'] = {i: float(score.text) for i, score in enumerate(review_scores)}
+            row['dates'] = {i: date.text for i, date in enumerate(review_dates)}
+            row['visit_num'] = {i: num.text for i, num in enumerate(visit_nums)}
+            print(f"{row['reviews']} \n {row['scores']}\n {row['dates']}\n {row['visit_num']}")
             sleep(0.5)
         except Exception as e:
             print(f'{e} ??')
+        final_rows.append(row)
 
-    dataframe = pd.DataFrame(data=df_rows)
+        if idx % save_period == 0:
+            dataframe = pd.DataFrame(data=final_rows)
+            save_path = save_dataframe(search + '_review', dataframe)
+            print(f"{idx}... {save_path} 저장완료")
+
+    dataframe = pd.DataFrame(data=final_rows)
     save_path = save_dataframe(search + '_review', dataframe)
     print(f"{save_path} 저장완료")
     return save_path
@@ -154,5 +169,5 @@ if __name__ == '__main__':
     # save_path = dataframe_loc_convert(save_path, 'address')
 
     save_path = 'data/2020-06-25_16_23_스타벅스dt.csv'
-    save_path = postprocess_df(save_path, 'address')
-    review_crawl(save_path)
+    save_path = postprocess_df(save_path, basis_name='address')
+    review_crawl(save_path, basis_column='address')
