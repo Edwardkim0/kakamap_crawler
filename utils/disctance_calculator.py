057af9f4748ddb06a0bb9120073025f6f6e2f160
dhkim <scminoov@gmail.com>
2020-07-06 11:13:14 +0900

    [Dev_Update] trip / starbucks distance calculator organize

--- utils/disctance_calculator.py
+++ utils/disctance_calculator.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils.data_process import read_csv
 
 
-def get_distances_with_df(df1, df2, df1_basis, df2_basis):
+def get_distances_with_df(df1, df2, df1_basis, df2_basis, df1_another: list = None, df2_another: list = None):
     rows = []
     for j in range(df1.shape[0]):
         loc_name1 = df1.loc[j][df1_basis]
@@ -19,6 +19,12 @@ def get_distances_with_df(df1, df2, df1_basis, df2_basis):
                     df2_basis: loc_name2,
                     'distance': distance
                 }
+                if df2_another is not None:
+                    for basis in df2_another:
+                        row[basis] = df2.loc[i][basis]
+                if df1_another is not None:
+                    for basis in df1_another:
+                        row[basis] = df1.loc[j][basis]
             except Exception as e:
                 print(e, df2.loc[i][df2_basis])
                 row = {
@@ -27,22 +33,25 @@ def get_distances_with_df(df1, df2, df1_basis, df2_basis):
                     'distance': 9999
                 }
             rows.append(row)
+        print(f'{j}th starbuck distance cal complete.')
     return rows
 
 
-def find_min_dist_starbucks(groupped_df, basis_name='star_name', min_numbers=2):
+def find_min_dist_starbucks(groupped_df, basis_name='star_name', min_numbers=2,
+                            target_keep_col=None):
     new_df = {}
     groupped_df = groupped_df.sort_values(by='distance').reset_index(drop=True)
     new_df['distance'] = groupped_df.loc[:min_numbers]['distance']
-    new_df[basis_name] = groupped_df.loc[:min_numbers][basis_name]
-    if min_numbers > 1:
-        print('this df contains column which has multiple list, please use [column_list_to_multiple_rows] function.')
-    return pd.Series(new_df, index=[basis_name, 'distance'])
+    for col in target_keep_col:
+        new_df[col] = groupped_df.loc[:min_numbers][col]
+    return pd.Series(new_df, index=target_keep_col + ['distance'])
 
 
-def column_list_to_multiple_rows(df, base_column: str, multi_column_names: list):
-    df_dict = {base_column: np.repeat(df[base_column].values, df[multi_column_names[0]].str.len())}
-    for column_name in multi_column_names:
+def column_list_to_multiple_rows(df, base_columns: list, multi_column_names: list):
+    df_dict = {}
+    for base_column in base_columns:
+        df_dict[base_column] = np.repeat(df[base_column].values, df[multi_column_names[0]].str.len())
+    for idx, column_name in enumerate(multi_column_names):
         df_dict[column_name] = np.concatenate(df[column_name].values)
     return pd.DataFrame(df_dict)
 
@@ -71,14 +80,30 @@ def distance_calculate(source_df, target_df, save_name, distance_number=1):
     :param distance_number:
     :return:
     """
+    if len(set(target_df.columns).intersection(set(source_df.columns))):
+        rename_map = {}
+        for col_name in list(target_df.columns):
+            if col_name in list(source_df.columns) and (col_name not in ['latitude', 'longitude']):
+                rename_map[col_name] = col_name + '2'
+        target_df = target_df.rename(columns=rename_map)
     source_basis = source_df.columns[0]
     target_basis = target_df.columns[0]
-    rows = get_distances_with_df(df1=source_df, df2=target_df, df1_basis=source_basis, df2_basis=target_basis)
+    source_keep_col = ['address']
+    target_keep_col = list(set(target_df.columns) - set(['latitude', 'longitude', target_basis]))
+    rows = get_distances_with_df(df1=source_df, df2=target_df, df1_basis=source_basis, df2_basis=target_basis,
+                                 df1_another=source_keep_col, df2_another=target_keep_col)
     src_tar_distance = pd.DataFrame(rows)
-    src_tar_distance2 = src_tar_distance.groupby([source_basis]).apply(
-        lambda x: find_min_dist_starbucks(x, source_basis, distance_number))
+    src_tar_distance.to_csv(f'data/{save_name}_distance_raw.csv', sep='\t')
+
+    src_tar_distance = pd.read_csv('data/Starbucks_btw_Tourist_distance_raw.csv', sep='\t')
+    src_tar_distance = src_tar_distance.drop('Unnamed: 0', axis=1)
+    source_cols = [source_basis] + source_keep_col
+    target_cols = target_keep_col + [target_basis]
+
+    src_tar_distance2 = src_tar_distance.groupby(source_cols).apply(
+        lambda x: find_min_dist_starbucks(x, source_basis, distance_number, target_cols))
     if distance_number > 1:
-        src_tar_distance2 = column_list_to_multiple_rows(src_tar_distance2, source_basis, [target_basis, 'distance'])
+        src_tar_distance2 = column_list_to_multiple_rows(src_tar_distance2, source_cols, ['distance'] + target_cols)
     src_tar_distance2 = src_tar_distance2.reset_index()
     src_tar_distance2.to_csv(f'data/{save_name}_distance.csv', sep='\t')
 
@@ -101,5 +126,6 @@ def main_startbucks_apt():
 
 if __name__ == '__main__':
     import os
+
     os.chdir('..')
     main_startbucks_tourist()
